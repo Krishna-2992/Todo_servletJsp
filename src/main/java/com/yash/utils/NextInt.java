46efa73a00317c85d85dd5faea2043301c3440a0
Krishna <krishnaagrawal2992@gmail.com>
2024-08-27 11:48:03 +0530

    fix: nextInt id provider

--- src/main/java/com/yash/utils/NextInt.java
+++ src/main/java/com/yash/utils/NextInt.java
@@ -1,11 +1,28 @@
 package com.yash.utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.yash.pojo.Task;
+
 public class NextInt {
 	public static int idCount = 0; 
 	
-	public static int getNextInt() {
-		idCount++;
-		System.out.println("Inside getNextInt with int as: " + idCount);
-		return idCount;
+	public static int getNextInt() throws SQLException {
+		Connection connection = DBConnection.getConnection();	
+		String q = "SELECT COUNT(*) FROM Tasks";
+		PreparedStatement ps = connection.prepareStatement(q);
+		ResultSet rs = ps.executeQuery();
+		
+	    if (rs.next()) {
+	        int count = rs.getInt(1); // Get the value from the first column (COUNT(*))
+	        
+	        System.out.println("Row count: " + count);
+	        return count+1;
+	    } else {
+	        throw new SQLException("No rows returned");
+	    }
 	}
 }
--- src/main/webapp/TodoList.jsp
+++ src/main/webapp/TodoList.jsp
@@ -14,7 +14,7 @@
         <form method="post" action="TodoController">
             <div class="mb-4">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Task Name:</label>
-                <input type="text" id="name" name="name" value="eat" 
+                <input type="text" id="name" name="name" 
                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                        placeholder="Enter Task Name">
             </div>
